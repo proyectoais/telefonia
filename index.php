7673496903e48ecbcf01740df0a10ae79d02140c
Hailyx Montenegro <nazareth_zapata16@hotmail.com>
2014-02-28 16:38:06 -0430

    Primera carga

---
+++ index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+<title>MIH C.A.</title>
+<link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+	<div id="contenedor">
+		<div id="encabezado">
+			<img src="imagen/logo.png">
+			<label id="titulo">Central Telef&oacute;nica MIH C.A.</label>
+		</div>
+		<hr>
+		<div id="contenido">
+			<ul class="menu">
+
+			<li><a href="#">Simular llamada</a>
+				<ul>
+					<li><a href="#" class="documents">Tradicional</a></li>
+					<li><a href="#" class="messages">Enrutador PBX</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Gestionar l&iacute;nea</a></li>
+			<li><a href="#">Plan</a>
+				<ul>
+					<li><a href="#" class="documents">Agregar</a></li>
+					<li><a href="#" class="messages">Campiar</a></li>
+					<li><a href="#" class="signout">Eliminar</a></li>
+				</ul>
+			</li>
+			<li><a href="#">SVA</a>
+				<ul>
+					<li><a href="#" class="documents">Agregar</a></li>
+					<li><a href="#" class="messages">Campiar</a></li>
+					<li><a href="#" class="signout">Eliminar</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Reportes</a>
+				<ul>
+					<li ><a href="#" class="documents">Mayor cantidad llamadas</a></li>
+					<li ><a href="#" class="documents">Record llamada por mes</a></li>
+					<li ><a href="#" class="documents">Cantidad de conferencias</a></li>
+					<li ><a href="#" class="documents">Plan m&aacute;s activado</a></li>
+					<li ><a href="#" class="documents">SVA m&aacute;s activado</a></li>
+					<li ><a href="#" class="documents">Plan menos activado</a></li>
+					<li ><a href="#" class="documents">SVA menos activado</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Registros</a></li>
+		</ul>
+		</div>
+
+		<!--<div id="pie">
+			<p>Desarrollado por Mar&iacute; Malav&eacute;, Isthar Zapata yHailyx Montenegro</p>
+		</div>-->
+  		
+	</div>
+</body>
+</html>
