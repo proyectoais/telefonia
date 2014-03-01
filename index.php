3c6ae80fb41d2c0472c67e618d3733069955f6b8
Hailyx Montenegro <nazareth_zapata16@hotmail.com>
2014-03-01 10:00:47 -0430

    bla

--- index.php
+++ index.php
@@ -12,9 +12,40 @@
 		</div>
 		<hr>
 		<div id="contenido">
-		<?php
-  			include("head.html");
-  		?>
+					<ul class="menu">
+
+			<li><a href="#">Simular llamada</a>
+				<ul>
+					<li><a href="normal.php" class="documents">Tradicional</a></li>
+					<li><a href="enrutador.php" class="messages">Enrutador PBX</a></li>
+				</ul>
+			</li>
+			<li><a href="gest_linea.php">Gestionar l&iacute;nea</a></li>
+			<li><a href="#">Plan</a>
+				<ul>
+					<li><a href="activarplan.php" class="documents">Activar</a></li>
+					<li><a href="desactivarplan.php" class="documents">Desactivar</a></li>
+				</ul>
+			</li>
+			<li><a href="#">SVA</a>
+				<ul>
+					<li><a href="activarsva.php" class="documents">Activar</a></li>
+					<li><a href="desactivarsva.php" class="documents">Desactivar</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Reportes</a>
+				<ul>
+					<li ><a href="#" class="documents">Mayor cantidad llamadas</a></li>
+					<li ><a href="#" class="documents">Record llamada por mes</a></li>
+					<li ><a href="#" class="documents">Cantidad de conferencias</a></li>
+					<li ><a href="#" class="documents">Plan m&aacute;s activado</a></li>
+					<li ><a href="#" class="documents">SVA m&aacute;s activado</a></li>
+					<li ><a href="#" class="documents">Plan menos activado</a></li>
+					<li ><a href="#" class="documents">SVA menos activado</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Registros</a></li>
+		</ul>
 		</div>
 
 		<!--<div id="pie">
